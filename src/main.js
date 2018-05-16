58477a4935be651a4417cfd4b00fb4f3c441ac7c
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-05-16 15:43:19 +0200

    Proof of concept of Pryv Authorization

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
+import Vuetify from "vuetify";
 import App from "./App.vue";
 import router from "./router";
+
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-content>
+<v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-title primary-title>
+            <h3 class="headline mb-0">Pryv</h3>
+        </v-card-title>
+        <v-card-actions>
+          <span id="pryv-button"></span>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+  </v-content>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+var pryv = require("pryv");
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  mounted: function() {
+    var credentials = null;
+    var pryvDomain = "pryv.me";
+    var requestedPermissions = [{
+      streamId: "*",
+      level: "manage"
+    }];
+
+    var settings = {
+      requestingAppId: "sempryv",
+      requestedPermissions: requestedPermissions,
+      spanButtonID: "pryv-button",
+      callbacks: {
+        signedIn: function (authData) {
+          credentials = authData;
+          // ...
+        },
+      }
+    };
+
+    pryv.Auth.config.registerURL.host = "reg." + pryvDomain;
+    pryv.Auth.setup(settings);
+    var connection = new pryv.Connection(credentials);
+    console.log(connection)
   }
 };
 </script>
