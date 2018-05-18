b414ddb9c1dcbf769756d341b340dc11b0fea834
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-05-18 11:49:18 +0200

    Display streams as JSON in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
               <h3 class="headline mb-0">Pryv</h3>
           </v-card-title>
           <v-card-text>
-            {{streams}}
+            <pre>{{streams}}</pre>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -32,8 +32,24 @@ export default {
       var conn = auth.connection();
       var vm = this;
 
-      conn.fetchStructure(function(err, streams) {
-        vm.streams = String(streams);
+      var options = {
+        state: null
+      };
+      conn.streams.get(options, function(err, streams) {
+        var fstreams = streams.map(function(stream) {
+          var obj = {
+            name: stream.name
+          };
+          if (stream.children.length) {
+            obj.children = stream._children.map(function(children) {
+              return {
+                name: children.name
+              };
+            });
+          }
+          return obj;
+        });
+        vm.streams = JSON.stringify(fstreams, null, 4);
       });
     }
   }
