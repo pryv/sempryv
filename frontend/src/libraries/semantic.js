aa1d2bc54684f985f332e9ebfd4f27a3d83c4512
Fabien Dubosson <fabien.dubosson@gmail.com>
2018-09-04 11:03:24 +0200

    Set namespace as a const in semantic.js, and add cleanup of clientdata

--- frontend/src/libraries/semantic.js
+++ frontend/src/libraries/semantic.js
@@ -1,5 +1,7 @@
 import auth from "@/auth";
 
+const namespace = "sempryv:codes";
+
 function update_stream(object, callback) {
   var conn = auth.connection();
   conn.streams.update(object, function(err) {
@@ -11,21 +13,24 @@ export function add_code(object, type, code, callback) {
   if (!object.clientData) {
     object.clientData = {};
   }
-  if (!object.clientData["sempryv:codes"]) {
-    object.clientData["sempryv:codes"] = {};
+  if (!object.clientData[namespace]) {
+    object.clientData[namespace] = {};
   }
-  if (!object.clientData["sempryv:codes"][type]) {
-    object.clientData["sempryv:codes"][type] = [];
+  if (!object.clientData[namespace][type]) {
+    object.clientData[namespace][type] = [];
   }
-  object.clientData["sempryv:codes"][type].push(code);
+  object.clientData[namespace][type].push(code);
   update_stream(object, callback);
 }
 
 export function del_code(object, type, code, callback) {
-  var index = object.clientData["sempryv:codes"][type].indexOf(code);
-  object.clientData["sempryv:codes"][type].splice(index, 1);
-  if (object.clientData["sempryv:codes"][type].length == 0) {
-    delete object.clientData["sempryv:codes"][type];
+  var index = object.clientData[namespace][type].indexOf(code);
+  object.clientData[namespace][type].splice(index, 1);
+  if (object.clientData[namespace][type].length == 0) {
+    delete object.clientData[namespace][type];
+  }
+  if (Object.keys(object.clientData[namespace]).length == 0) {
+    object.clientData[namespace] = null;
   }
   update_stream(object, callback);
 }
