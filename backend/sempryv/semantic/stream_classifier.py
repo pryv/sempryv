f5f5ccea8dd06824179d049063cabee790e39686
orfi2017 <orfeas.aidonopo@hevs.ch>
2019-11-06 21:17:20 +0100

    modify classifier for training on synthetic data, on progress

--- backend/sempryv/semantic/stream_classifier.py
+++ backend/sempryv/semantic/stream_classifier.py
@@ -2,16 +2,51 @@ import pickle
 from semantic.stream_parser import StreamParser
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+import json
 
 
 class StreamsClassifier(object):
     def __init__(self, users_data=list):
         self.train_data = []
+        self.train_data_synthetic = []
         self.target_data = []
+        self.target_data_synthetic = []
         self.users_data = users_data
         self.code_labels = {}
-        self.ontology_names = {"SNOMEDCT": "snomed-ct", "LOINC": "loinc"}
+        self.ontology_names = {"SNOMEDCT": "snomed-ct", "LOINC": "loinc", "LNC": "loinc"}
         self.count_vect = CountVectorizer()
+        # self.synthetics = self.load_synthetic_data()
+        # self.create_train_data_from_synthetics()
+
+    def load_synthetic_data(self):
+        f_thryve = open('synthetics/thryve-streams.json', 'r')
+        f_thryve_annotations = open('synthetics/data_annotated-thryve-filtered.json', 'r')
+        thryve_streams = json.load(f_thryve)
+        d_stream_types = self.get_name_types(thryve_streams)
+        thryve_annotated_data = json.load(f_thryve_annotations)
+        for ann_dato in thryve_annotated_data:
+            type = d_stream_types[ann_dato['id']]
+            name = ann_dato['name']
+            annotations = ann_dato['codes']
+            self.train_data_synthetic.append(name + ' ' + type)
+            codes = ''
+            for code in annotations:
+                annotation = code['id'].split('/')[-1]
+                ontology_name = self.ontology_names[code['id'].split('/')[-2]]
+                codes += self.ontology_names[annotation['system_name']] + ':' + annotation['code'] + '_'
+            label = self.assign_codes_label(codes)
+            self.target_data_synthetic.append(label)
+
+        f_thryve.close()
+        f_thryve_annotations.close()
+        x = 1
+
+    def get_name_types(self, streams: []) -> dict:
+        dict_types = {}
+        for stream in streams:
+            client_data = stream['clientData']
+            dict_types[stream['id']] = list(client_data['sempryv:codes'].keys())[0]
+        return dict_types
 
     def create_train_data(self):
         for user in self.users_data:
@@ -35,6 +70,7 @@ class StreamsClassifier(object):
         self.save_dict_to_file(self.code_labels)
 
     def create_train_data_from_synthetics(self):
+
         streams = []
         for stream in streams:
             stream_name = stream['name']
@@ -83,9 +119,12 @@ class StreamsClassifier(object):
             if label == predicted:
                 return codes
 
-# users_data = [{'uname': 'orfi2019', 'token': 'cjxa7szlr00461id327owwz27'},
-#               {'uname': 'orfeas', 'token': 'cjzy2ioal04xj0e40zdcy4sku'}]
-# sc = StreamsClassifier(users_data=users_data)
+
+users_data = [{'uname': 'orfi2019', 'token': 'cjxa7szlr00461id327owwz27'},
+              {'uname': 'orfeas', 'token': 'cjzy2ioal04xj0e40zdcy4sku'}]
+sc = StreamsClassifier(users_data=users_data)
+sc.load_synthetic_data()
+
 # # sc.create_train_data()
 # sc.train()
 # new_stream = ["ACtivity txt"]
